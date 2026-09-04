1e542facc87e8efec127a58c10a1b76fde537ad0
baiwumm <me@baiwumm.com>
2026-09-04 16:03:15 +0800

    feat(next): 配置图片优化选项以支持远程图片加载
    - 添加 remotePatterns 配置以允许从 Supabase 加载远程图片
    - 设置 unoptimized 为 true 以禁用 Vercel 图片优化服务
    - 解决图片资源无法正常显示的问题

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL("https://cbqzqhiqjasshpmunpmo.supabase.co/**")],
+    unoptimized: true, // 禁用 Vercel 图片优化
+  },
+};
 
 export default nextConfig;
